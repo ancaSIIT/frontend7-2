c94e37d655193d56da51531e590cbf882c168803
Adriana Cente <adrianacente12@gmail.com>
2019-03-04 20:26:06 +0200

    modif button

--- views/movieDetails.js
+++ views/movieDetails.js
@@ -5,7 +5,7 @@ function onHtmlLoaded() {
     var container=document.getElementById("movieDetails");
     var movie=new Movie({_id:id});
     movie.getMovie().then(function() {
-        container.innerHTML="<div id='info'><h1 id='title'>"+movie.Title+"</h1><p id='year'>("+movie.Year+")</p>"+
+        container.innerHTML="<div class='info'><h2 id='title'>"+movie.Title+"</h2><p id='year'>("+movie.Year+")</p>"+
         "<p id='imdbVotes'> IMDb votes: <br />"+movie.imdbVotes+"</p><p id='imdbRating' title='Rating IMDb'>"+
         "<i class='fas fa-star' style='font-size:25px;color:yellow'></i>&nbsp;&nbsp;"+movie.imdbRating+"</p>"+
         "<div style='clear:both'></div><div class='line'><p id='runtime'>"+movie.Runtime+"</p><p id='genre'>"+movie.Genre+"</p>"+
@@ -21,7 +21,7 @@ function onHtmlLoaded() {
         "</label>"+movie.Type+"</p><p id='dvd' class='datails'><label>DVD: </label>"+movie.DVD+"</p><p id='boxoffice' class='datails'>"+
         "<label>BoxOffice: </label>"+movie.BoxOffice+"</p><p id='production' class='datails'><label>Production: "+
         "</label>"+movie.Production+"</p><p id='website' class='datails'><label>Website: </label>"+movie.Website+"</p>"+
-        "<p id='response' class='datails'><label>Response: </label>"+movie.Response+"</p><div id='btns'><button id='editMovie'>"+
+        "<p id='response' class='datails'><label>Response: </label>"+movie.Response+"</p><div class='btns' id="+movie._id+"><button id='editMovie'>"+
         "Edit movie</button><button id='deleteMovie'>Delete movie</button></div>";
         console.log(movie);
 
