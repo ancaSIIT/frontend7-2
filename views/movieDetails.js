b10b4701f803ac219afeb1fb61888402c51a2b4d
Adriana Cente <adrianacente12@gmail.com>
2019-03-08 16:13:23 +0200

    search

--- views/movieDetails.js
+++ views/movieDetails.js
@@ -10,19 +10,15 @@ function onHtmlLoaded() {
         "<i class='fas fa-star' style='font-size:25px;color:yellow'></i>&nbsp;&nbsp;"+movie.imdbRating+"</p>"+
         "<div style='clear:both'></div><div class='line'><p id='runtime'>"+movie.Runtime+"</p><p id='genre'>"+movie.Genre+"</p>"+
         "<p id='released'>"+movie.Released+"</p></div><br /><div style='clear:both'></div></div><div id='image'>"+
-        "<img id='poster' src='"+movie.Poster+"' /></div><p id='rated' class='datails'><label>Rated: </label>"+movie.Rated+"</p>"+
-        "<p id='director' class='datails'><label>Director: </label>"+movie.Director+"</p><p id='writer' class='datails'>"+
-        "<label>Writer: </label>"+movie.Writer+"</p><p id='actors' class='datails'><label>Actors: </label>"+movie.Actors+"</p>"+
-        "<p id='plot' class='datails'><label>Plot: </label>"+movie.Plot+"</p><p id='language' class='datails'><label>Language: "+
-        "</label>"+movie.Language+"</p><p id='country' class='datails'><label>Country: </label>"+movie.Country+"</p>"+
-        "<p id='awards' class='datails'><label>Movies: </label>"+movie.Awards+"</p><label id='ratingLabel'>Ratings: </label>"+
-        "<ul id='ratings' class='datails'></ul><p id='metascore' class='datails'><label>Metascore: </label>"+movie.Metascore+"</p>"+
-        "<p id='imdbID' class='datails'><label>IMDb ID: </label>"+movie.imdbID+"</p><p id='type' class='datails'><label>Type: "+
-        "</label>"+movie.Type+"</p><p id='dvd' class='datails'><label>DVD: </label>"+movie.DVD+"</p><p id='boxoffice' class='datails'>"+
-        "<label>BoxOffice: </label>"+movie.BoxOffice+"</p><p id='production' class='datails'><label>Production: "+
-        "</label>"+movie.Production+"</p><p id='website' class='datails'><label>Website: </label>"+movie.Website+"</p>"+
-        "<p id='response' class='datails'><label>Response: </label>"+movie.Response+"</p><div class='btns' id="+movie._id+"><button id='editMovie'>"+
-        "Edit movie</button><button id='deleteMovie'>Delete movie</button></div>";
+        "<img id='poster' src='"+movie.Poster+"' alt='"+movie.Title+"'/></div><p id='rated' class='datails'><label>Rated:&nbsp;</label>"+movie.Rated+"</p>"+
+        "<p id='director' class='datails'><label>Director:&nbsp;</label>"+movie.Director+"</p><p id='writer' class='datails'>"+
+        "<label>Writer:&nbsp;</label>"+movie.Writer+"</p><p id='actors' class='datails'><label>Actors:&nbsp;</label>"+movie.Actors+"</p>"+
+        "<p id='plot' class='datails'><label>Plot:&nbsp;</label>"+movie.Plot+"</p><p id='language' class='datails'><label>Language:&nbsp;"+
+        "</label>"+movie.Language+"</p><p id='country' class='datails'><label>Country:&nbsp;</label>"+movie.Country+"</p>"+
+        "<p id='awards' class='datails'><label>Movie awards:&nbsp;</label>"+movie.Awards+"</p><label id='ratingLabel'>Ratings:&nbsp;</label>"+
+        "<ul id='ratings' class='datails'></ul><p id='metascore' class='datails'><label>Metascore:&nbsp;</label>"+movie.Metascore+"</p>"+
+        "<p id='imdbID' class='datails'><label>IMDb ID:&nbsp;</label>"+movie.imdbID+"</p><p id='type' class='datails'><label>Type:&nbsp;"+
+        "</label>"+movie.Type+"</p><div class='btns' id="+movie._id+"><button id='editMovie'>Edit</button><button id='deleteMovie'>Delete</button></div>";
         console.log(movie);
 
         var list=document.getElementById('ratings');
